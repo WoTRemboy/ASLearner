376b7c14bb431c2d56726915c7dee1a7b6446956
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-12-16 17:29:15 +0300

    Initial Commit

---
+++ ASLearner/ASLearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLearnerApp.swift
+//  ASLearner
+//
+//  Created by Roman Tverdokhleb on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ASLearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
